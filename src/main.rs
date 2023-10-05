91ab7e76002a2b32774af686a4e283b6aac24526
SnowCharm <619022098@qq.com>
2023-10-05 15:28:24 +0800

    :sparkles: print prompt messages

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use demo_console_grep::print;
+use demo_console_grep::system;
 
 fn main() {
-    print::print_info("Hello, world!");
+    print::print_info("Welcome to demo-console-grep!");
+    print::print_info(format!("OS: {}", system::get_os()).as_str());
+    print::print_info(format!("Version: {}", system::get_version()).as_str());
 }
---
+++ src/system.rs
@@ -0,0 +1,46 @@
+enum SystemType {
+    _MacOS,
+    _LinuxOS,
+    _WinOS,
+    _UnknownOS,
+}
+
+#[cfg(target_os = "_MacOS")]
+const SYS_TYPE: SystemType = SystemType::_MacOS;
+#[cfg(target_os = "linux")]
+const SYS_TYPE: SystemType = SystemType::_LinuxOS;
+#[cfg(target_os = "windows")]
+const SYS_TYPE: SystemType = SystemType::_WinOS;
+#[cfg(not(any(target_os = "_MacOS", target_os = "linux", target_os = "windows")))]
+const SYS_TYPE: SystemType = SystemType::_UnknownOS;
+
+/// Get OS info
+///
+/// # Examples
+///
+/// ```
+/// use demo_console_grep::system;
+/// let os = system::get_os();
+/// assert_ne!(os, "Unknown");
+/// ```
+pub fn get_os() -> String {
+    match SYS_TYPE {
+        SystemType::_MacOS => String::from("MacOS"),
+        SystemType::_LinuxOS => String::from("Linux"),
+        SystemType::_WinOS => String::from("Windows"),
+        SystemType::_UnknownOS => String::from("Unknown"),
+    }
+}
+
+/// Get version info
+/// 
+/// # Examples
+/// 
+/// ```
+/// use demo_console_grep::system;
+/// let version = system::get_version();
+/// println!("{}", version);
+/// ```
+pub fn get_version() -> String {
+    env!("CARGO_PKG_VERSION").to_string()
+}
