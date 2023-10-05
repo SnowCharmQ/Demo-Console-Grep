d3675fb2a4b6c4e8dfb6abb236137009592b362f
SnowCharm <619022098@qq.com>
2023-10-05 15:37:41 +0800

    :bug: fix bug in printing info

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use demo_console_grep::print;
 use demo_console_grep::system;
 
 fn main() {
+    println!("{}", "=".repeat(40));
     print::print_info("Welcome to demo-console-grep!");
     print::print_info(format!("OS: {}", system::get_os()).as_str());
     print::print_info(format!("Version: {}", system::get_version()).as_str());
--- src/print.rs
+++ src/print.rs
@@ -7,14 +7,15 @@
 /// print::print_info("Hello, world!");
 /// ```
 pub fn print_info(str: &str) {
-    let mut info = String::new();
     if str.len() < 20 {
         let padding = 20 - str.len();
         let padding_str = " ".repeat(padding / 2);
-        info = format!("{}{}{}", padding_str, str, padding_str);
+        let mut info = format!("{}{}{}", padding_str, str, padding_str);
         if padding % 2 != 0 {
             info = format!("{} ", info);
         }
+        println!("========= {:<20} =========", info);
+    } else {
+        println!("{}", format!("{:^40}", str));
     }
-    println!("========= {:<20} =========", info);
 }
